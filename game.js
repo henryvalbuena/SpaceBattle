4bed455c3078f8749f48a38d86a7e9ca42290e77
DevHenry <dev.henryvm@gmail.com>
2017-12-28 06:57:16 -0600

    miniBoss fight added

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
-var bonusBox, bonusStats, scoreBoard, airCraft, aliens, laserOne, laseDual, crashed,
-    bulletTime, background0, background1, reset, innerW,
-    innerH, scoreCount;
+var bonusBoxGreen, bonusBoxBlue, bonusStats, scoreBoard, airCraft, aliens,
+    laser, timedEvents, background0, background1, reset, innerW, innerH;
 /****************
 GAME START CALLOUT: Calls for the canvas and canvas elements functions
 this is the function to be called to start the game
 ****************/
-function startGame() {
-  scoreCount = 0;
+function startGame(){
   aliens = [];
-  bonusStats = {dual: false, shield: false, nuke: false};
-  bulletTime = crashed = false;
+  laser = [];
+  bonusStats = {single: true, dual: false, blast:false, shield: false, nuke: false};
+  timedEvents = {bulletTime: false, bossShotsCount: 0, scoreCount: 0, resetButton: false}
   innerH = window.innerHeight;
   innerW = window.innerWidth;
 // Component(width, Height, Color or Imange, x, y, type)
@@ -19,14 +18,15 @@ function startGame() {
   for(var i = 0; i <= 5; i++){
     aliens[i] = new Component(40, 40, "AlienShip.png", 0, 0, "image");
   }
-// Component(radius, cuts, color, x, y, type)
-  laserOne = new Component(4, 0, "rgba(56, 247, 218, 0.8)", innerW/2, innerH/2, "laser");
-  laserDual = new Component(4, 0, "rgba(56, 247, 218, 0.8)", innerW/2, innerH/2, "laser");
+  for(var i = 0; i <= 6; i++){
+    laser[i] = new Component(0, 0, "laserRed01.png", -50, -50, "image");
+  }
   reset = new Component(100, 50, "Button.png", 30, 30, "image");
   scoreBoard = new Component(80, 30, "gray", (innerW/2 - innerW/3), (innerH - 100), "text");
-  bonusBox = new Component(30,30, "bonusB.png", 70, -40, "image");
+  bonusBoxGreen = new Component(30,30, "powerupGreen_bolt.png", 70, -40, "image");
+  bonusBoxBlue = new Component(30,30, "powerupBlue_bolt.png", 70, -40, "image");
   myGameArea.start();
-  myGameArea.refresh();
+  // myGameArea.refresh();
 }
 /****************
 CANVAS: Create canvas, added event listener and frame rate function call, stop or reset
@@ -37,41 +37,53 @@ var myGameArea = {
     this.canvas.width = innerW - 20;
     this.canvas.height = innerH - 20;
     this.context = this.canvas.getContext("2d");
+    this.canvas.style.cursor = "none";
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    window.addEventListener('keypress', (event) => {
+    document.addEventListener('keypress', (event) => {
       if(event.keyCode == 32) {
-        bulletTime = true;
+        timedEvents.bulletTime = true;
       }
     });
-    document.addEventListener('click', (event) => {
-      var buttonBott = reset.x + reset.width;
-      var buttonSide = reset.y + reset.height;
-      if(event.clientX >= reset.x && event.clientX <= buttonBott
-        && event.clientY <= buttonSide && event.clientY >= reset.y){
-        myGameArea.refresh();
-      }
-    });
+      document.addEventListener('click', (event) => {
+        var buttonBott = reset.x + reset.width;
+        var buttonSide = reset.y + reset.height;
+        if((event.clientX >= reset.x && event.clientX <= buttonBott)
+          && (event.clientY <= buttonSide && event.clientY >= reset.y)
+          && timedEvents.resetButton){
+          startGame();
+          timedEvents.resetButton = false;
+        }
+      });
     document.addEventListener('mousemove', (event) =>{
       airCraft.x = event.pageX;
       airCraft.y = event.pageY;
     });
+    this.interval = setInterval(updateGameArea, 20);
   },
   clear: function(){
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
   stop: function(){
     reset.create();
+    timedEvents.resetButton = true;
     this.canvas.style.cursor = "";
     clearInterval(this.interval);
   },
-  refresh: function() {
-    this.canvas.style.cursor = "none";
-    scoreCount = 0;
-    bulletTime = false;
-    laserOne.x = airCraft.x = innerW/2;
-    laserOne.y = airCraft.y = innerH/2;
-    this.interval = setInterval(updateGameArea, 20);
-    aliens.forEach((x) => x.alienHit());
+  gameEvents: function(){
+    //should be set to show a miniBoss every 70 points
+    if (timedEvents.bossShotsCount == 20) {
+      timedEvents.bossShotsCount++;
+      return false;
+    } else if (timedEvents.bossShotsCount == 41) {
+      return false;
+    } else if (timedEvents.scoreCount >= 60 && (aliens[5].y + aliens[5].height) <= innerH) {
+      return true;
+    } else if (timedEvents.scoreCount >= 30 && (aliens[5].y + aliens[5].height) <= innerH
+  && timedEvents.bossShotsCount <= 20) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 /****************
@@ -90,17 +102,12 @@ function Component(width, height, c, x, y, type){
   this.angle = 0;
   this.create = function() {
     ctx = myGameArea.context;
-    if(type == "image"){
+  if(type === "image"){
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    } else if(type == "laser"){
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.width, this.height, 2*Math.PI);
-      ctx.fillStyle = c;
-      ctx.fill();
-    } else if(type == "text"){
+    } else if(type === "text"){
       ctx.font = "15px Arial";
       ctx.fillStyle = "white";
-      ctx.fillText("Score "+scoreCount,this.x, this.y);
+      ctx.fillText("Score "+timedEvents.scoreCount,this.x, this.y);
     }
   }
   this.collision = function(obj) {
@@ -112,10 +119,6 @@ function Component(width, height, c, x, y, type){
       var thisLeft = this.x;
       var thisRight = this.x + this.width;
       var thisTop = this.y;
-      if(bulletTime){
-        thisBott += 15;
-        thisRight+= 2;
-      }
       if((((objTop <= thisBott && objTop >= thisTop)
       || (objBott >= thisTop && objBott <= thisBott))
       && (objLeft <= thisRight && objRight >= thisLeft))){
@@ -124,33 +127,75 @@ function Component(width, height, c, x, y, type){
   		return false;
       }
   }
-  this.fire = function(type) {
-    if(bulletTime && (this.y >= 0)){
-      this.y-=22;
+  this.laseMotion = function(type) {
+    if(timedEvents.bulletTime && (this.y >= 0)){
+      this.y-= 38;
+      this.width = 10;
+      this.height = 40;
     } else {
-      if(type === "single" && !bonusStats.dual){
-        this.x = airCraft.x + airCraft.width/2;
+      if((type === "single") && bonusStats.single){
+        this.image.src = "laserRed01.png";
+        this.x = airCraft.x - 5 + airCraft.width/2;
         this.y = airCraft.y - 5;
-      } else if(type === "dual" && bonusStats.dual){
-        this.x = airCraft.x+47;
-        this.y = airCraft.y+25;
-      } else if(type === "single" && bonusStats.dual){
-        this.x = airCraft.x+13;
-        this.y = airCraft.y+25;
+      } else if((type === "dual") && bonusStats.dual){
+        this.image.src = "laserGreen11.png";
+        this.x = airCraft.x + 43;
+        this.y = airCraft.y + 25;
+      } else if((type === "single") && bonusStats.dual){
+        this.image.src = "laserGreen11.png";
+        this.x = airCraft.x + 9;
+        this.y = airCraft.y + 25;
+      } else if((type === "single") && bonusStats.blast){
+        this.image.src = "laserBlue15.png";
+        this.x = airCraft.x - 5 + airCraft.width/2;
+        this.y = airCraft.y - 5;
+      } else if((type === "dual") && bonusStats.blast){
+        this.image.src = "laserBlue15.png";
+        this.x = airCraft.x + 9;
+        this.y = airCraft.y + 25;
+      } else if((type === "blast") && bonusStats.blast){
+        this.image.src = "laserBlue15.png";
+        this.x = airCraft.x + 43;
+        this.y = airCraft.y + 25;
       }
-      bulletTime = false;
+      timedEvents.bulletTime = false;
+      this.width = 0;
+      this.height = 0;
     }
   }
-  this.alienHit = function() {
-    this.x = Math.floor(Math.random()*(window.innerWidth - 30)+1);
-    this.y = -40;
-  }
-  this.alienMotion = function(){
-    this.angle  += 1 * Math.PI / 15;
-    this.y++;
-    if(this.y >= innerH){
-      scoreCount--;
+  this.alienHit = function(mode) {
+    if(mode === "bossStart"){
+      this.x = Math.floor(Math.random()*(window.innerWidth - 40)+1);
+    }else if(mode === "alien"){
+      this.x = Math.floor(Math.random()*(window.innerWidth - 40)+1);
       this.y = -40;
+    }
+  }
+  this.alienMotion = function(mode){
+    if((this.y >= innerH) && (mode === "bossStart")){
+      this.alienHit("bossEnd");
+      myGameArea.stop();
+    }else if((this.y >= innerH) && (mode === "alien")){
+      timedEvents.scoreCount--;
+      this.alienHit(mode);
+    }else if((this.y <= innerH) && (mode === "bossStart")){
+      this.width = 100;
+      this.height = 100;
+      this.image.src = "ship4_mod.png";
+      this.y += 0.5;
+      if(this.y == -30){this.x = Math.floor(Math.random()*(innerW*0.8)+5)}
+      // if(timedEvents.miniBossMove <= 30){
+      //   this.x+=5;
+      //   timedEvents.miniBossMove++;
+      // }else{
+      //   this.x-=5;
+        // timedEvents.miniBossMove--;
+      // }
+    }else if(mode === "bossEnd"){
+      this.y = -40;
+      this.width = this.height = 0;
+    }else{
+      this.y++;
     }
   }
   this.looping = function(){
@@ -161,12 +206,11 @@ function Component(width, height, c, x, y, type){
       background0.y = 0;
     }
   }
-  this.glow = function(){
-    if(((scoreCount >= 20) && !bonusStats.dual)
-    || ((scoreCount >= 60) && !bonusStats.dual)
-    || ((scoreCount >= 100) && !bonusStats.dual)){
+  this.boxShow = function(){
+    if(((timedEvents.scoreCount >= 20 && timedEvents.scoreCount <= 50) && bonusStats.single)
+    || ((timedEvents.scoreCount >= 50 && timedEvents.scoreCount <= 90) && bonusStats.dual)){
       if(this.y <= innerH+this.height){
-        if(this.y == -20){this.x = Math.floor(Math.random()*(innerW*0.8)+5)}
+        if(this.y == -30){this.x = Math.floor(Math.random()*(innerW*0.8)+5)}
         this.y++;
       }else{
         this.y = -40;
@@ -175,14 +219,19 @@ function Component(width, height, c, x, y, type){
       this.y = -40;
     }
   }
+  this.aliensLaser = function(){
+
+  }
 }
 /****************
-GAME AREA UPDATE: this is the timer function
+GAME AREA UPDATE: setInterval function
 ****************/
 function updateGameArea() {
   if(aliens.some((x) =>{
   return airCraft.collision(x)})){
+    bonusStats.single = true;
     bonusStats.dual = false;
+    bonusStats.blast = false;
     myGameArea.stop();
   } else {
     myGameArea.clear();
@@ -190,27 +239,60 @@ function updateGameArea() {
     background1.create();
     background1.looping();
     airCraft.create();
-    if(bonusStats.dual){
-      laserDual.create();
-      laserDual.fire("dual");
-    }
-    laserOne.create();
-    laserOne.fire("single");
     scoreBoard.create();
-    bonusBox.create();
-    bonusBox.glow();
-    if(bonusBox.collision(airCraft)){
+    if(bonusBoxGreen.collision(airCraft)){
       bonusStats.dual = true;
+      bonusStats.single = false;
+      bonusStats.blast = false;
+      bonusBoxGreen.boxShow();
+    }else if(bonusBoxBlue.collision(airCraft)){
+      bonusStats.dual = false;
+      bonusStats.single = false;
+      bonusStats.blast = true;
+      bonusBoxBlue.boxShow();
     }
-    aliens.forEach((x) =>{
-      x.create();
-      x.alienMotion();
-      if(laserOne.collision(x) || laserDual.collision(x) && bulletTime) {
-        bulletTime = false;
-        scoreCount++;
-        x.alienHit();
-      }
-    });
+    if(bonusStats.single){
+      bonusBoxGreen.create();
+      bonusBoxGreen.boxShow();
+      laser[0].create();
+      laser[0].laseMotion("single");
+    } else if(bonusStats.dual){
+      bonusBoxBlue.create();
+      bonusBoxBlue.boxShow();
+      laser[0].create();
+      laser[0].laseMotion("single");
+      laser[1].create();
+      laser[1].laseMotion("dual");
+    }else if(bonusStats.blast){
+      laser[0].create();
+      laser[0].laseMotion("single");
+      laser[1].create();
+      laser[1].laseMotion("dual");
+      laser[2].create();
+      laser[2].laseMotion("blast");
+    }
+    if(myGameArea.gameEvents()){
+      aliens[aliens.length-1].create();
+      aliens[aliens.length-1].alienMotion("bossStart");
+    }else{
+      aliens[aliens.length-1].alienMotion("bossEnd");
+    }
+    for(var i = 0; i <= aliens.length-2; i++){
+        aliens[i].create();
+        aliens[i].alienMotion("alien");
+        if((laser.some((y) =>{ return y.collision(aliens[i])}))
+        && timedEvents.bulletTime) {
+          timedEvents.bulletTime = false;
+          timedEvents.scoreCount++;
+          aliens[i].alienHit("alien");
+        }
+    }
+    if((laser.some((y) =>{ return y.collision(aliens[aliens.length-1])}))
+    && timedEvents.bulletTime && myGameArea.gameEvents()){
+      timedEvents.bulletTime = false;
+      timedEvents.bossShotsCount++;
+      aliens[aliens.length-1].alienHit("bossStart");
+    }
   }
 }
 startGame();
